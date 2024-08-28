d922fd155d331c3bdc6251f91cac8cc9d2979102
bhataasim1 <bhataasim700@gmail.com>
2024-08-28 14:50:44 +0530

    feat: Enable strict mode and add image domains and headers configuration
    The code changes enable strict mode in Next.js configuration and add image domains and headers configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: [
+            '127.0.0.1',
+        ],
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
