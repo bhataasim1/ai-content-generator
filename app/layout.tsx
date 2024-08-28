8c3ca105a438f3ea751d8222cedd97066d0a9625
bhataasim1 <bhataasim700@gmail.com>
2024-08-28 15:26:28 +0530

    chore: Update layout.tsx to include ClerkLoaded component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, GoogleOneTap } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkProvider, GoogleOneTap } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <ClerkProvider>
         <GoogleOneTap />
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}><ClerkLoaded>{children}</ClerkLoaded></body>
       </ClerkProvider>
     </html>
   );
