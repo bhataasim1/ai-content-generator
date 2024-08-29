cd24f422d4140b16465914952e753a33ec61ddbc
bhataasim1 <bhataasim700@gmail.com>
2024-08-29 12:31:10 +0530

    refactor: Update Header component to use next/image for logo

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
-// import Image from "next/image";
-// import ALOGO from "@/public/ALogo.svg"
+import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="flex justify-between p-5 shadow-sm">
-      {/* <Image src={ALOGO} alt="logo" width={150} height={100} priority /> */}
-      <h1 className="font-bold text-primary text-2xl italic">AI Course Generator</h1>
+      <Image src={'/vercel.svg'} alt="logo" width={150} height={100} priority />
+      {/* <h1 className="font-bold text-primary text-2xl italic">AI Course Generator</h1> */}
       <Button>Get Started</Button>
     </div>
   );
