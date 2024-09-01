d15fb85b3bfb60047caa0a05bf53d5503f3a200f
bhataasim1 <bhataasim700@gmail.com>
2024-09-01 13:19:43 +0530

    feat: Update BaseEnvironment to include DRIZZLE_DATABASE_URL configuration

--- configs/BaseEnvironment.ts
+++ configs/BaseEnvironment.ts
@@ -1,8 +1,13 @@
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
 export type Environment = "development" | "production" | "test";
 
 export class BaseEnvironment {
   defaultEnvironmentValues = {
     GOOGLE_GEMENI_API_KEY: "my-api-key",
+    DRIZZLE_DATABASE_URL: "postgresql://myuser:mypassword@mydbhost.com/mydatabase",
   };
 
   get environment(): Environment {
@@ -13,6 +18,13 @@ export class BaseEnvironment {
     return (
       process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY! ||
       this.defaultEnvironmentValues.GOOGLE_GEMENI_API_KEY
+    );
+  }
+
+  get DRIZZLE_DATABASE_URL(): string {
+    return (
+      process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL! ||
+      this.defaultEnvironmentValues.DRIZZLE_DATABASE_URL
     );
   }
 }
