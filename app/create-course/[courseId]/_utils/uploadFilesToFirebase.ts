9844ee03c834912616fb7baaeafe380c350b4612
bhataasim1 <bhataasim700@gmail.com>
2024-09-06 16:04:56 +0530

    Refactor uploadFilesToFirebase function to allow image upload and update course banner URL

---
+++ app/create-course/[courseId]/_utils/uploadFilesToFirebase.ts
@@ -0,0 +1,31 @@
+import { db } from "@/configs/db";
+import { firebaseStorage } from "@/configs/firebase.config";
+import { CourseList } from "@/schema/schema";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { CourseType } from "../page";
+import { eq } from "drizzle-orm";
+
+export const uploadFilesToFirebase = async (
+  file: Blob,
+  courseInfo: CourseType
+) => {
+  const fileName = `${Date.now()}-${courseInfo?.courseId!}-${
+    courseInfo?.category
+  }.jpg`;
+  const storageRef = ref(firebaseStorage, "ai-content-generator/" + fileName);
+  await uploadBytes(storageRef, file)
+    // .then((snapshot) => {
+    //     console.log("Image Uploaded Successfully", snapshot);
+    // })
+    .then(() => {
+      getDownloadURL(storageRef).then(async (url) => {
+        // console.log("File available at", url);
+        await db
+          .update(CourseList)
+          .set({
+            courseBanner: url,
+          })
+          .where(eq(CourseList.id, courseInfo?.id!));
+      });
+    });
+};
