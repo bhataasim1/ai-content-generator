faeb6bb5eaaa20d3af22d947d94b8d446f0a8edf
bhataasim1 <bhataasim700@gmail.com>
2024-09-10 16:59:30 +0530

    Refactor import statement in EditCourseBasicInfo component

--- app/create-course/[courseId]/_components/_edit/EditCourseBasicInfo.tsx
+++ app/create-course/[courseId]/_components/_edit/EditCourseBasicInfo.tsx
@@ -14,11 +14,11 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { DialogClose } from "@radix-ui/react-dialog";
 import { FaEdit } from "react-icons/fa";
-import { CourseType } from "../../page";
 import { useEffect, useState } from "react";
 import { db } from "@/configs/db";
 import { CourseList } from "@/schema/schema";
 import { eq } from "drizzle-orm";
+import { CourseType } from "@/types/types";
 
 type EditCourseBasicInfoProps = {
   courseInfo: CourseType | null;
