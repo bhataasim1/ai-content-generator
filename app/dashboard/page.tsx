e8e1bfe59f6e2b64a9ab08cbd39997c340c94d57
bhataasim1 <bhataasim700@gmail.com>
2024-09-10 16:59:57 +0530

    Refactor import statements in dashboard page component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import AddCourse from "./_components/AddCourse";
+import UserCourseList from "./_components/UserCourseList";
 
 const page = () => {
   return (
     <div>
       <AddCourse />
+      <UserCourseList />
     </div>
   );
 };
