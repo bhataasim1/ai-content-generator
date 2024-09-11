df4a0805c6cd81c9ae299f4e17181d4bedef2e71
bhataasim1 <bhataasim700@gmail.com>
2024-09-11 21:26:22 +0530

    Refactor UserCourseList context import statements and add UserCourseListContext provider

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,19 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { UserCourseListContext } from "../_context/UserCourseList.context";
 import Header from "./_components/Header";
 import Sidebar from "./_components/Sidebar";
+import { CourseType } from "@/types/types";
 
 const DashboardLayout = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
+  const [userCourseList, setUserCourseList] = useState<CourseType[]>([]);
   return (
-    <div>
-      <div className="md:w-64 hidden md:block">
-        <Sidebar />
+    <UserCourseListContext.Provider
+      value={{ userCourseList, setUserCourseList }}
+    >
+      <div>
+        <div className="md:w-64 hidden md:block">
+          <Sidebar />
+        </div>
+        <div className="md:ml-64">
+          <Header />
+          <div className="p-10">{children}</div>
+        </div>
       </div>
-      <div className="md:ml-64">
-        <Header />
-        <div className="p-10">{children}</div>
-      </div>
-    </div>
+    </UserCourseListContext.Provider>
   );
 };
 
