a50e1eb54c8f5e1831aaa1eadc000b82097fb33c
bhataasim1 <bhataasim700@gmail.com>
2024-09-11 21:26:29 +0530

    Refactor CourseCard component to include dropdown options for course management

--- app/dashboard/_components/CourseCard.tsx
+++ app/dashboard/_components/CourseCard.tsx
@@ -1,25 +1,55 @@
 import { CourseType } from "@/types/types";
 import Image from "next/image";
 import { MdMenuBook } from "react-icons/md";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import DropDownOptions from "./DropDownOptions";
+import { db } from "@/configs/db";
+import { CourseList } from "@/schema/schema";
+import { eq } from "drizzle-orm";
 
 type CourseCardProps = {
   course: CourseType;
+  onRefresh: () => void;
 };
 
-const CourseCard = ({ course }: CourseCardProps) => {
+const CourseCard = ({ course, onRefresh }: CourseCardProps) => {
   // console.log(course);
+
+  const handleOnDelete = async () => {
+    const res = await db
+      .delete(CourseList)
+      .where(eq(CourseList.id, course.id))
+      .returning({
+        id: CourseList.id,
+        courseName: CourseList.courseName,
+      });
+
+    if (res) {
+      onRefresh();
+    }
+  };
+
+  // hover:border-primary hover:scale-105 transition-all cursor-pointer mt-4
   return (
-    <div className="shadow-sm rounded-lg border p-2 hover:border-primary hover:scale-105 transition-all cursor-pointer mt-4">
+    <div className="shadow-sm rounded-lg border p-2 ">
       <Image
-        src={course.courseBanner!}
-        alt={course.courseName}
+        src={course?.courseBanner ?? "/vercel.svg"}
+        alt={course?.courseName ?? "Ai Course Generator"}
         width={300}
         height={200}
         priority
-        className="w-full h-[200px] object-cover rounded-lg"
+        className="w-full h-[200px] object-cover rounded-lg "
       />
       <div className="p-2">
-        <h2 className="font-medium text-lg">{course.courseOutput.topic}</h2>
+        <h2 className="font-medium text-lg flex items-center justify-between">
+          {course.courseOutput.topic}
+          <DropDownOptions handleDeleteCourse={() => handleOnDelete()}>
+            <HiOutlineDotsVertical
+              size={20}
+              className="cursor-pointer p-1 bg-purple-50 text-primary text-sm rounded-sm"
+            />
+          </DropDownOptions>
+        </h2>
         <p className="text-sm text-gray-400 my-1">{course.category}</p>
 
         <div className="flex items-center justify-between">
