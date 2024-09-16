5bd7b4a4f1a9f5ef7eab8ac6345f9d56d04c950b
bhataasim1 <bhataasim700@gmail.com>
2024-09-16 18:17:42 +0530

    Refactor ExplorePage component to display a paginated list of courses with user information

--- app/dashboard/explore/page.tsx
+++ app/dashboard/explore/page.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { db } from "@/configs/db";
+import { CourseList } from "@/schema/schema";
+import { CourseType } from "@/types/types";
+import React, { useEffect, useState } from "react";
+import CourseCard from "../_components/CourseCard";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+const ExplorePage = () => {
+  const [courseList, setCourseList] = useState<CourseType[] | null>(null);
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const getAllCourses = async () => {
+    const result = await db
+      .select()
+      .from(CourseList)
+      .limit(8)
+      .offset(pageIndex * 8);
+    // console.log(result);
+    setCourseList(result as CourseType[]);
+  };
+
+  console.log(courseList);
+
+  useEffect(() => {
+    getAllCourses();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageIndex]);
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h2 className="font-bold text-3xl">Explore More Courses</h2>
+      <p>Explore courses build with AI by Other Users</p>
 
-export default page
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
+        {courseList?.map((course) => (
+          <div key={course.courseId}>
+            <CourseCard
+              course={course}
+              onRefresh={() => getAllCourses()}
+              displayUser={true}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-between mt-5 items-center">
+        <Button
+          onClick={() => setPageIndex(pageIndex - 1)}
+          disabled={pageIndex == 0}
+        >
+          Prev
+        </Button>
+        <Badge>Page : {pageIndex + 1}</Badge>
+        <Button
+          onClick={() => setPageIndex(pageIndex + 1)}
+          disabled={courseList?.length !== 8}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ExplorePage;
