150343341d5bc63effd6265ee616212effc0d0ea
bhataasim1 <bhataasim700@gmail.com>
2024-09-17 19:26:14 +0530

    Update logo in Header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const { user } = useUser();
   return (
     <div className="flex justify-between p-5 shadow-sm">
-      <Image src={"/vercel.svg"} alt="logo" width={150} height={100} priority />
+      <Image src={"/logo.png"} alt="logo" width={150} height={100} priority className="object-cover" />
       {!user ? (
         <Link href="/sign-up">
           <ShinyButton text="Sign Up" />
