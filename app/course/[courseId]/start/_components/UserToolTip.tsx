01aeb2b40bd28b677aeccda91e473c3629942472
bhataasim1 <bhataasim700@gmail.com>
2024-09-17 18:17:22 +0530

    Add UserToolTip component to display user information in CourseStart

---
+++ app/course/[courseId]/start/_components/UserToolTip.tsx
@@ -0,0 +1,44 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Tooltip,
+  TooltipArrow,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Image from "next/image";
+import React from "react";
+
+const UserToolTip = ({
+  username,
+  userProfileImage,
+}: {
+  username: string;
+  userProfileImage: string;
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <p className="flex justify-center items-center gap-2">
+            This Course is by -
+            <Badge className="cursor-pointer">{username}</Badge>
+          </p>
+        </TooltipTrigger>
+        <TooltipContent variant={"secondary"}>
+          <Image
+            src={userProfileImage || "/vercel.svg"}
+            alt={username || "AI Course Generator"}
+            width={50}
+            height={50}
+            priority
+            className="rounded-full"
+          />
+          <TooltipArrow />
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default UserToolTip;
